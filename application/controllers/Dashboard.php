7951c47c516c0e89000e2757a1d31b06bfe71c47
Sholawati <85085687+sholawati@users.noreply.github.com>
2022-07-23 18:01:10 +0700

    Update Dashboard.php

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -6,7 +6,7 @@ class Dashboard extends CI_Controller
     public function __construct()
     {
         parent::__construct();
-        // is_logged_in();
+        is_logged_in();
         $this->load->model('Obat_model');
         $this->load->model('Penjualan_model');
         $this->load->model('User_model');
